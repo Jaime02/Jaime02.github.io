30a55fb65df7a4b17f8f2a26a411beaa19780c35
Jaime Resano <resano.138736@e.unavarra.es>
2025-01-16 01:05:45 +0100

    A mimir

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
+import glsl from "vite-plugin-glsl";
 import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
@@ -11,5 +9,18 @@ export default defineConfig({
   output: "static",
   site: 'https://jaime02.github.io',
   base: '/WebTaller/',
-  integrations: [tailwind(), mdx(), react()],
+  integrations: [tailwind(), react()],
+  vite: {
+    plugins: [glsl()],
+    build: {
+      assetsInlineLimit: 0,
+      rollupOptions: {
+        output: {
+          assetFileNames: '[ext]/[name][extname]',
+          entryFileNames: 'script/entry.js',
+        },
+      },
+      cssCodeSplit: false,
+    },
+  }
 });
